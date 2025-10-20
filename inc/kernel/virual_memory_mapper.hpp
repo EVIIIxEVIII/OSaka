ad2f4c41a54b78b3334f2de276bf3194b9c3cc8f
Kirill Katz <xnlyre@protonmail.com>
2025-10-20 11:10:30 +0300

    Make progress in virtual memory

--- inc/kernel/virual_memory_mapper.hpp
+++ inc/kernel/virual_memory_mapper.hpp
@@ -8,12 +8,30 @@ typedef struct {
     byte* page_table;
 } PageTables;
 
+typedef enum {
+    VM_RESERVED,
+    VM_FREE
+} VmRangeFlags;
 
 typedef struct VmRange {
     u64 start;
     u64 end;
+    VmRangeFlags flag;
     VmRange* next;
 } VmRange;
+
+typedef enum PageTableEntryFlags {
+    PAGE_TABLE_ENTRY_CLEAR_FLAGS = ~((1ULL << 12) - 1),
+    PAGE_TABLE_ENTRY_PRESENT_BIT = (1 << 0),
+    PAGE_TABLE_ENTRY_READ_WRITE_BIT = (1 << 1),
+    PAGE_TABLE_ENTRY_USER_SUPERVISOR_BIT = (1 << 2),
+} PageTableEntryFlags;
+
+typedef struct {
+    VmRange* base;
+    u64 count;
+    u64 capacity;
+} VmRangeTable;
 
 void vmm_init();
 byte* vmm_map(u64 size);
--- src/kernel/virtual_memory_mapper.cpp
+++ src/kernel/virtual_memory_mapper.cpp
@@ -1,37 +1,17 @@
 #include "kernel/virual_memory_mapper.hpp"
 #include "kernel/physical_memory_mapper.hpp"
 
+#define IDENTITY_MAP (RESERVED + 2*sizeof(VmRange))
 
-typedef enum PageTableEntryFlags {
-    PAGE_TABLE_ENTRY_CLEAR_FLAGS = ~((1ULL << 12) - 1),
-    PAGE_TABLE_ENTRY_PRESENT_BIT = (1 << 0),
-    PAGE_TABLE_ENTRY_READ_WRITE_BIT = (1 << 1),
-    PAGE_TABLE_ENTRY_USER_SUPERVISOR_BIT = (1 << 2),
-} PageTableEntryFlags;
+static VmRangeTable vm_range_table{};
 
-static VmRange vm_range{};
+static VmRange* vm_range_root{};
 static u64* page_global_directory = (u64*)pmm_alloc(4096);
 
-void vmm_init() {
-    vm_range.start = 0x0;
-    vm_range.end = (1ULL << 48) - 1;
-    vm_range.next = nullptr;
-}
-
-byte* vmm_map(u64 size) {
-    VmRange current = vm_range;
-
-    for (;;) {
-
-
-
-    }
-
-}
-
-u64* get_or_alloc_next(u64* table, u64 index) {
-    if (table[index] & 1) {
-        return (u64*)table[index];
+static u64* get_or_alloc_next(u64* table, u64 index) {
+    if (table[index] & PAGE_TABLE_ENTRY_PRESENT_BIT) {
+        u64 entry = table[index];
+        return (u64*)(entry & PAGE_TABLE_ENTRY_CLEAR_FLAGS);
     }
 
     u64* next_table_address = (u64*)pmm_alloc(4096);
@@ -45,18 +25,61 @@ u64* get_or_alloc_next(u64* table, u64 index) {
     return next_table_address;
 }
 
-void map_page(u64 virt, u64 phys) {
+static void map_page(u64 virt, u64 phys) {
     u64 page_global_dir_i = (virt >> 39) & 0x1FF;
     u64 page_upper_dir_i = (virt >> 30) & 0x1FF;
     u64 page_middle_dir_i = (virt >> 21) & 0x1FF;
     u64 page_table_i = (virt >> 12) & 0x1FF;
 
-
     u64* page_upper_dir = get_or_alloc_next(page_global_directory, page_global_dir_i);
     u64* page_middle_dir = get_or_alloc_next(page_upper_dir, page_upper_dir_i);
     u64* page_table = get_or_alloc_next(page_middle_dir, page_middle_dir_i);
 
-    u64 page_table_entry = page_table[page_table_i];
+    u64 page_table_entry = phys;
+    page_table_entry |= PAGE_TABLE_ENTRY_PRESENT_BIT;
+    page_table_entry |= PAGE_TABLE_ENTRY_READ_WRITE_BIT;
+
+    page_table[page_table_i] = page_table_entry;
+}
+
+//static u64
+
+static void map_identity_pages() {
+    for (u64 addr = 0x0; addr < IDENTITY_MAP; addr += PAGE_SIZE) {
+        map_page(addr, addr);
+    }
+}
+
+void vmm_init() {
+
+    map_identity_pages();
+
+    VmRange* vm_range = (VmRange*)pmm_alloc(4096);
+    vm_range->start = 0x0;
+    vm_range->end = (u64)IDENTITY_MAP;
+    vm_range->flag = VM_RESERVED;
+    vm_range->next = nullptr;
+
+    VmRange* vm_range_next = vm_range+1;
+    vm_range_next->start = (u64)IDENTITY_MAP;
+    vm_range_next->end = (u64)((1ULL << 48) - 1);
+    vm_range_next->flag = VM_FREE;
+    vm_range_next->next = nullptr;
+
+    vm_range->next = vm_range_next;
+    vm_range_root = vm_range;
+
+    vm_range_table.base = vm_range;
+    vm_range_table.count = 2;
+    vm_range_table.capacity = PAGE_SIZE / sizeof(VmRange);
+}
+
+byte* vmm_map(u64 size) {
+
+
+}
+
+void vmm_free(u64 virt) {
 
 
 }
